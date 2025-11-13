f8faf63f3d6a4ee62d4ff00f7dd113b0f44bfb61
tangfire <52628216@qq.com>
2025-11-13 23:07:04 +0800

    day1-05 jsx基础 实现列表渲染

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
 // 项目的根组件
 // App -> index.js -> public/index.html(root)
 
-const count = 100
-
-function getName(){
-    return 'jack'
-}
+const list = [
+    {id:1001,name:"John Doe"},
+    {id:1002,name:"January"},
+    {id:1003,name:"July"},
+]
 
 function App() {
   return (
     <div className="App">
-     this is App
-        {/*使用引号传递字符串*/}
-        {'this is message '}
-        {/*识别js变量*/}
-        {count}
-        {/*函数调用*/}
-        {getName()}
-        {/*方法调用*/}
-        {new Date().getDate()}
-        {/*使用js对象*/}
-        <div style={{
-            color: 'red',
-        }}>
-            this is div
-        </div>
+     this is app
+        <ul>
+            {list.map(item => <li key={item.id}>{item.name}</li>)}
+        </ul>
     </div>
   );
 }
