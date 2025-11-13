4d3d60e4926718815f2a1f5e5c90154167b9ac26
tangfire <52628216@qq.com>
2025-11-13 23:03:45 +0800

    day1-04 jsx基础 识别js表达式

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// 项目的根组件
+// App -> index.js -> public/index.html(root)
+
+const count = 100
+
+function getName(){
+    return 'jack'
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     this is App
+        {/*使用引号传递字符串*/}
+        {'this is message '}
+        {/*识别js变量*/}
+        {count}
+        {/*函数调用*/}
+        {getName()}
+        {/*方法调用*/}
+        {new Date().getDate()}
+        {/*使用js对象*/}
+        <div style={{
+            color: 'red',
+        }}>
+            this is div
+        </div>
     </div>
   );
 }
