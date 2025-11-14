6908975138a5390b561c1dead408b5b9f1e73381
tangfire <52628216@qq.com>
2025-11-14 21:23:46 +0800

    day1 React中的事件绑定

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 // 项目的根组件
 // App -> index.js -> public/index.html(root)
 
-const articleType = 3 // 0 1 3
+// const handleClick = () => {
+//   console.log('button clicked');
+// }
 
-// 定义核心函数(根据文章类型返回不同的JSX模版)
-function getArticleTem() {
-  if (articleType === 0){
-    return <div>我是无图文章</div>
-  }else if (articleType === 1){
-    return <div>我是单图模式</div>
-  }else{
-    return <div>我是三图模型</div>
-  }
+// const handleClick = (e) => {
+//   console.log('button clicked: ',e);
+// }
+
+// const handleClick = (name) => {
+//     console.log('button clicked: ',name);
+// }
+
+const handleClick = (name,e) => {
+    console.log('button clicked: ',name,e);
 }
 
 function App() {
   return (
 
     <div className="App">
-      {getArticleTem()}
+     <button onClick={(e) => handleClick('jack',e)}>click me</button>
     </div>
 
 
