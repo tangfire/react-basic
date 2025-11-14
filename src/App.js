2c9dc9bc88700f2968761210c950bf30a16cd2c0
tangfire <52628216@qq.com>
2025-11-14 21:41:18 +0800

    day1 useState基础使用

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 // 项目的根组件
 // App -> index.js -> public/index.html(root)
 
-// function Button(){
-//     return <button>click me</button>
-// }
-
-const Button = () =>{
-    return <button>click me!</button>
-}
+// useState实现一个计数器按钮
+import {useState} from "react";
 
 function App() {
+
+  // count 状态变量
+  // setCount 修改状态变量的方法
+  const [count, setCount] = useState(0)
+
+  // 2. 点击事件回调
+  const handleClick = () => {
+    setCount(count + 1)
+  }
   return (
 
     <div className="App">
-     <Button></Button>
-        <br/>
-        <Button/>
+    <button onClick={handleClick}>{count}</button>
     </div>
 
 
