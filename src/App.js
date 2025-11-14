3513c32584ad6fba498ebbe1abcda7a41b9d459a
tangfire <52628216@qq.com>
2025-11-14 21:12:44 +0800

    day1-05 jsx基础 实现复杂条件渲染

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 // 项目的根组件
 // App -> index.js -> public/index.html(root)
 
-const isLogin = false
+const articleType = 3 // 0 1 3
+
+// 定义核心函数(根据文章类型返回不同的JSX模版)
+function getArticleTem() {
+  if (articleType === 0){
+    return <div>我是无图文章</div>
+  }else if (articleType === 1){
+    return <div>我是单图模式</div>
+  }else{
+    return <div>我是三图模型</div>
+  }
+}
 
 function App() {
   return (
 
     <div className="App">
-      {/*逻辑与 &&*/}
-        {isLogin && <span>this is span</span>}
-      {/*三元运算*/}
-      {isLogin ? <span>jack</span>: <span>loading</span>}
+      {getArticleTem()}
     </div>
 
 
