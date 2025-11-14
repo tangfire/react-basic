025a6f8f5164edfd6a03b2374825809f0112af38
tangfire <52628216@qq.com>
2025-11-14 21:52:15 +0800

    day1 useState修改状态的规则

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 // 项目的根组件
 // App -> index.js -> public/index.html(root)
 
-// useState实现一个计数器按钮
 import {useState} from "react";
 
 function App() {
 
-  // count 状态变量
-  // setCount 修改状态变量的方法
-  const [count, setCount] = useState(0)
+  let [count, setCount] = useState(0);
 
-  // 2. 点击事件回调
   const handleClick = () => {
-    setCount(count + 1)
+    setCount(count + 1);
   }
+
+  const [form,setForm] = useState({
+    name:'jack'
+  })
+
+  const changeForm = () => {
+    setForm({
+      ...form,
+      name:'jack216'
+    });
+  }
+
   return (
 
     <div className="App">
-    <button onClick={handleClick}>{count}</button>
+      <button onClick={handleClick}>{count}</button>
+      <button onClick={changeForm}>修改form {form.name}</button>
     </div>
 
 
