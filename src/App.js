252cf8fc796301b0e14ddae8895abe8fbcde066b
tangfire <52628216@qq.com>
2025-11-15 15:41:09 +0800

    day2 React中获取DOM

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 // 项目的根组件
 // App -> index.js -> public/index.html(root)
 
-import {useState} from 'react'
+import {useRef} from 'react'
+
+// 1. useRef生成ref对象,绑定到dom标签身上
+// 2. dom可用时,ref.current获取don
+// 渲染完毕之后dom生成之后才可用
 
 function App() {
-  const [value, setValue] = useState('');
+  const inputRef = useRef(null)
+  const showDom = () =>{
+    console.log(inputRef.current)
+    console.dir(inputRef.current)
+  }
   return (
 
     <div className="App">
-      <input value={value} onChange={(e)=>{
-          setValue(e.target.value)
-          console.log(value)
-      }} type="text"/>
+      <input type="text" ref={inputRef} />
+        <button onClick={showDom}>获取dom</button>
     </div>
 
 
