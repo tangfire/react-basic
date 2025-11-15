58d89ee0cefbea45a8c7d7906b06636e75202ac7
tangfire <52628216@qq.com>
2025-11-15 16:31:41 +0800

    day2 组件通信-父传子-children说明

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@
 function Son(props){
     console.log(props)
     // props: 对象里面包含了父组件传递过来的所有数据
-  return <div>this is son {props.name}, jsx: {props.child}</div>
+  return <div>this is son {props.children}</div>
 }
 
 
@@ -20,16 +20,9 @@ function App() {
   return (
 
     <div className="App">
-      <Son
-          name={name}
-          age={18}
-          isTrue={true}
-          list={['vue','react']}
-          obj={{name:'jack'}}
-          cb={()=>{console.log(123)}}
-          child={<span>this is span</span>}
-
-      ></Son>
+      <Son>
+          <span>this is span</span>
+      </Son>
     </div>
 
 
