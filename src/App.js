89d3b940df2e3fb3b2e96f4cf60a11a774394e7b
tangfire <52628216@qq.com>
2025-11-29 15:53:18 +0800

    day10 useReducer

--- src/App.js
+++ src/App.js
@@ -1,44 +1,38 @@
 // 项目的根组件
 // App -> index.js -> public/index.html(root)
 
+// 1. 定义reducer函数 根据不同的action 返回不同的状态
+import {useReducer} from "react";
 
+function reducer(state, action) {
+    switch (action.type) {
+        case 'INC':
+            return state + 1
+        case 'DEC':
+            return state - 1
+        case 'SET':
+            return action.payload
+        default:
+            return state
+    }
+}
 
-import { useSelector,useDispatch } from 'react-redux'
-import { decrement, increment,addToNum } from './store/modules/counterStore'
-import {fetchChannelList} from "./store/modules/channelStore";
-import {useEffect} from "react";
+// 2. 组件里面调用useReducer(reducer,0) => [state,dispatch]
+
+// 3. 调用dispatch({type:'INC'}) => 通过reducer产生一个新的状态 使用这个新状态更新UI
 
 function App() {
-    const count = useSelector(state => state.counter.count)
-    const channelList = useSelector(state => state.channel.channelList)
-    const dispatch = useDispatch()
-    // 使用useEffect触发异步请求执行
-    useEffect(() => {
-        dispatch(fetchChannelList())
-    })
+    const [state,dispatch] = useReducer(reducer,0);
 
   return (
       <div className="App">
-          <div>
-              <button
-                  aria-label="Increment value"
-                  onClick={() => dispatch(increment())}
-              >
-                  Increment
-              </button>
-              <span>{count}</span>
-              <button
-                  aria-label="Decrement value"
-                  onClick={() => dispatch(decrement())}
-              >
-                  Decrement
-              </button>
-              <button onClick={()=>dispatch(addToNum(10))}>add To 10</button>
-              <button onClick={()=>dispatch(addToNum(20))}>add To 20</button>
-              <ul>
-                  {channelList.map(item => <li key={item.id}>{item.name}</li>)}
-              </ul>
-          </div>
+        this is {state}
+          <br/>
+          <button onClick={()=>dispatch({type:'INC'})}>+</button>
+          <br/>
+          <button onClick={()=>dispatch({type:'DEC'})}>-</button>
+          <br/>
+          <button onClick={()=>dispatch({type:'SET',payload:100})}>100</button>
       </div>
   );
 }
